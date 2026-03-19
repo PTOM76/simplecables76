18ad1e1c4f4f8381941bf0816b2186edf67b475a
PTOM76 <ptms76@outlook.jp>
2026-03-19 22:35:24 +0900

    Add EnergyCable class and associated JSON models for rendering and properties

--- src/main/kotlin/net/pitan76/simplecables76/block/EnergyCable.kt
+++ src/main/kotlin/net/pitan76/simplecables76/block/EnergyCable.kt
@@ -1,16 +1,29 @@
 package net.pitan76.simplecables76.block
 
+import net.pitan76.mcpitanlib.api.block.CompatBlockRenderType
 import net.pitan76.mcpitanlib.api.block.CompatWaterloggable
+import net.pitan76.mcpitanlib.api.block.args.RenderTypeArgs
 import net.pitan76.mcpitanlib.api.block.v2.CompatibleBlockSettings
+import net.pitan76.mcpitanlib.api.event.block.AppendPropertiesArgs
 import net.pitan76.mcpitanlib.api.event.block.BlockPlacedEvent
 import net.pitan76.mcpitanlib.api.event.block.BlockUseEvent
 import net.pitan76.mcpitanlib.api.event.block.StateReplacedEvent
 import net.pitan76.mcpitanlib.api.event.block.TileCreateEvent
+import net.pitan76.mcpitanlib.api.state.property.CompatProperties
 import net.pitan76.mcpitanlib.api.tile.CompatBlockEntity
 import net.pitan76.mcpitanlib.api.util.CompatActionResult
 import net.pitan76.mcpitanlib.api.util.TextUtil
 
-class EnergyCable(settings: CompatibleBlockSettings) : AbstractCable(settings), CompatWaterloggable {
+class EnergyCable : AbstractCable, CompatWaterloggable {
+
+    constructor(settings: CompatibleBlockSettings) : super(settings) {
+        setDefaultState(defaultMidohraState.with(CompatProperties.UP, false)
+            .with(CompatProperties.DOWN, false)
+            .with(CompatProperties.NORTH, false)
+            .with(CompatProperties.EAST, false)
+            .with(CompatProperties.SOUTH, false)
+            .with(CompatProperties.WEST, false))
+    }
 
     override fun onRightClick(e: BlockUseEvent): CompatActionResult {
         val blockEntity = e.blockEntity
@@ -44,5 +57,15 @@ class EnergyCable(settings: CompatibleBlockSettings) : AbstractCable(settings), 
 
     override fun isTick(): Boolean {
         return true
+    }
+
+    override fun appendProperties(args: AppendPropertiesArgs) {
+        super.appendProperties(args)
+        args.addProperty(CompatProperties.UP, CompatProperties.DOWN, CompatProperties.NORTH,
+            CompatProperties.EAST, CompatProperties.SOUTH, CompatProperties.WEST)
+    }
+
+    override fun getRenderType(args: RenderTypeArgs?): CompatBlockRenderType {
+        return CompatBlockRenderType.MODEL
     }
 }
